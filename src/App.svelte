ff65e75a70b08b60cee0142ef7640b9a75aa1535
Brandon Endres <17bendres@gmail.com>
2020-09-18 14:35:08 -0400

    Adds simple nav bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,14 @@
 <script lang="ts">
+  import Nav from "./Nav.svelte";
 </script>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: var(--accent-color);
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  div {
+    height: 100%;
+    width: 100%;
   }
 </style>
 
-<main>
-  <h1>Soon to be...</h1>
-</main>
+<div>
+  <Nav />
+</div>
---
+++ src/Nav.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<style>
+  nav {
+    background-color: var(--default-primary-color);
+    display: flex;
+  }
+
+  h1 {
+    color: var(--text-icons);
+    padding: 1rem;
+    font-size: 32px;
+  }
+</style>
+
+<nav>
+  <h1>Brandon Endres</h1>
+</nav>
