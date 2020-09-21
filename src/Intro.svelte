79931dd6957da73c4e8b9c215b8275d724d565c1
Brandon Endres <17bendres@gmail.com>
2020-09-21 17:10:31 -0400

    Adds an intro section with nav bar

---
+++ src/Intro.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<style>
+    .intro {
+      display: flex;
+      width: 100%;
+      height: 75vh;
+      background-color: var(--default-primary-color);
+      align-items: center;
+    }
+  </style>
+  
+  <div class="intro" id="intro">
+
+  </div>
